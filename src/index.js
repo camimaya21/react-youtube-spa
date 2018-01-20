fa2ad3fef26ec6df3e196d87ee69043b30ac16c5
Maya Salas Maria Camila <mariacamila.maya@realtech.es>
2018-01-20 15:37:13 +0100

    .env and Api Key fixed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {YT_API_KEY} from 'babel-dotenv';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search-bar'
 
-const API_KEY = 'process.env.REACT_APP_DEV_YOUTUBE_API';
+YTSearch({key: YT_API_KEY, term: 'paramore'}, function(data){
+  console.log(data);
+});
 
 const App = () => {
   return (
