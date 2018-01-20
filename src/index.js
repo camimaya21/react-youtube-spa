8be9e4e1d81ec76948f4700e980e5e9b7c910bdd
Maya Salas Maria Camila <mariacamila.maya@realtech.es>
2018-01-20 17:07:13 +0100

    video list ready

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import {YT_API_KEY} from 'babel-dotenv';
 import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
 
-import SearchBar from './components/search-bar'
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-YTSearch({key: YT_API_KEY, term: 'paramore'}, function(data){
-  console.log(data);
-});
+    this.state = { videos: [] };
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+    YTSearch({key: YT_API_KEY, term: 'snowboarding'}, (videos) => {
+      this.setState({ videos });
+    }); 
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'))
