afb6514770a4584d914fde7c67c752c8568263c2
camimaya21 <camilamaya21@gmail.com>
2018-03-18 21:29:44 +0100

    seach bar works

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -8,10 +8,15 @@ class SearchBar extends Component {
 
     render(){
       return (
-        <div>
-            <input onChange={ e => this.setState({ term: e.target.value })} />;
+        <div className="search-bar">
+            <input onChange={ e => this.onInputChange(e.target.value)} />
          </div>
      );
+    }
+
+    onInputChange(term){
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 }
 
